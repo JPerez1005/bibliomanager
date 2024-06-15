99bdcd758dd1084567f74bd4765143ec01eb792b
JPerez1005 <perezbuenojuliandavid@gmail.com>
2024-06-14 23:42:52 -0500

    "actualización de datos"

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/js/**/*.{vue,js,ts,jxs,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
