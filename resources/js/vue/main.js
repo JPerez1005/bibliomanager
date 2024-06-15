788ff406b0fad4d632e9eeaedb1098fce391ff42
JPerez1005 <perezbuenojuliandavid@gmail.com>
2024-06-14 23:45:33 -0500

    "actualizacion"

--- resources/js/vue/main.js
+++ resources/js/vue/main.js
@@ -1,1 +1,13 @@
+import { createApp } from "vue";
+import App  from './App.vue';
+import axios from "axios";
+import router from './router.js';
+
 import '../../css/vue.css';
+
+const app=createApp(App);
+
+app.config.globalProperties.$axios = axios;
+window.axios=axios;
+
+app.mount("#app");
